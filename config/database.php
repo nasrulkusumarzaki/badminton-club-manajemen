25e0cfcfa9041415f730056017046c7a8ece8490
Nasrul Kusumarzaki <nasrulzaki10@gmail.com>
2026-08-27 10:09:49 +0700

    Fix fatal error class Pdo\Mysql di koneksi mariadb

--- config/database.php
+++ config/database.php
@@ -87,7 +87,9 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            (defined('Pdo\Mysql::ATTR_SSL_CA')
+            ? \Pdo\Mysql::ATTR_SSL_CA
+            : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
